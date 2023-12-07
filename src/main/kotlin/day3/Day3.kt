af66af95aeaaa509e799114e535388591fbb31a6
Emre Alkan <alkan@harver.com>
2023-12-07 14:46:10 +0100

    AoC2023 Day3/Part1 Input file has been added

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,14 @@
+package day3
+
+import readFile
+
+fun main() {
+
+    fun part1(lines: List<String>): Int {
+
+        return 0
+    }
+
+    val input = readFile("day3/Part1")
+    println("Part One: ${part1(input)}")
+}
