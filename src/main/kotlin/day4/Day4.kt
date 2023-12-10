0f0c8fb5fd049169b4f6370e40e991f24cf655c2
Emre Alkan <alkan@harver.com>
2023-12-11 00:47:10 +0100

    AoC2023 Day4/Part1

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -1,11 +1,27 @@
 package day4
 
 import readFile
+import kotlin.math.pow
+
+val WHITESPACE_PATTERN = "\\s+".toRegex()
 
 fun main() {
 
     fun part1(lines: List<String>): Int {
-        return 0
+        return lines.map { line ->
+            val (_, numbers) = line.split(':')
+            val (winningNumbers, yourNumbers) = numbers.split("|")
+            val winningList = winningNumbers.trim().split(WHITESPACE_PATTERN)
+            val yourNumberList = yourNumbers.trim().split(WHITESPACE_PATTERN)
+
+            val matchedNumbers = yourNumberList.count { it in winningList }
+            var matchedPoints = 0
+
+            matchedPoints = if ( matchedNumbers == 0) 0
+            else 2.0.pow(matchedNumbers.toDouble() - 1).toInt()
+
+            matchedPoints
+        }.reduce { acc, i -> acc + i }
     }
 
     val lines = readFile("day4/Part1")
