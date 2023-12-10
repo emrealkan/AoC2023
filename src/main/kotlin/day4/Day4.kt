d198676ef3bf9d7ed7f6f8822f9945f47baa8dfb
Emre Alkan <alkan@harver.com>
2023-12-10 23:28:54 +0100

    AoC2023 Day4/Part1 Input file has been added

---
+++ src/main/kotlin/day4/Day4.kt
@@ -0,0 +1,13 @@
+package day4
+
+import readFile
+
+fun main() {
+
+    fun part1(lines: List<String>): Int {
+        return 0
+    }
+
+    val lines = readFile("day4/Part1")
+    println("Part One: ${part1(lines)}")
+}
