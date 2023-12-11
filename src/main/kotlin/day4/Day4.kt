4abc573e4ca194f74d33b4506a110ce8aa6665d3
Emre Alkan <alkan@harver.com>
2023-12-11 20:33:10 +0100

    AoC2023 Day4/Part2

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -4,26 +4,56 @@ import readFile
 import kotlin.math.pow
 
 val WHITESPACE_PATTERN = "\\s+".toRegex()
+val CARD_PATTERN = "Card\\s+(?<cardId>\\d+)".toRegex()
 
 fun main() {
 
+    fun getTotalMatchedNumber(line: String): Int {
+        val (_, numbers) = line.split(':')
+        val (winningNumbers, yourNumbers) = numbers.split("|")
+        val winningList = winningNumbers.trim().split(WHITESPACE_PATTERN)
+        val yourNumberList = yourNumbers.trim().split(WHITESPACE_PATTERN)
+
+        return yourNumberList.count { it in winningList }
+    }
+
     fun part1(lines: List<String>): Int {
         return lines.map { line ->
-            val (_, numbers) = line.split(':')
-            val (winningNumbers, yourNumbers) = numbers.split("|")
-            val winningList = winningNumbers.trim().split(WHITESPACE_PATTERN)
-            val yourNumberList = yourNumbers.trim().split(WHITESPACE_PATTERN)
+            val matchedNumbers = getTotalMatchedNumber(line)
 
-            val matchedNumbers = yourNumberList.count { it in winningList }
-            var matchedPoints = 0
-
-            matchedPoints = if ( matchedNumbers == 0) 0
+            val matchedPoints: Int = if (matchedNumbers == 0) 0
             else 2.0.pow(matchedNumbers.toDouble() - 1).toInt()
 
             matchedPoints
         }.reduce { acc, i -> acc + i }
     }
 
-    val lines = readFile("day4/Part1")
+    fun part2(lines: List<String>): Int {
+        val cardIdToMatchedNumber = lines.map { line ->
+            val (cardInfo, _) = line.split(':')
+            val (cardId) = CARD_PATTERN.find(cardInfo)!!.destructured
+            val matchedNumbers = getTotalMatchedNumber(line)
+
+            cardId to matchedNumbers
+        }
+
+        val allCards = Array(cardIdToMatchedNumber.size) { 1 }
+        for (index in allCards.indices) {
+            val matchedNumbers = cardIdToMatchedNumber[index].second
+            if (matchedNumbers > 0) {
+                for (m in 0 until matchedNumbers) {
+                    val nextIndex = index + m + 1
+                    if (nextIndex < allCards.size) {
+                        allCards[nextIndex] += allCards[index]
+                    }
+                }
+            }
+        }
+
+        return allCards.sum()
+    }
+
+    val lines = readFile("day4/Part1&2")
     println("Part One: ${part1(lines)}")
+    println("Part Two: ${part2(lines)}")
 }
