992c361c6fb6df9b368b22a3d442420ed03887c6
Emre Alkan <alkan@harver.com>
2023-12-13 00:25:23 +0100

    AoC2023 Day6/Part1

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -2,9 +2,34 @@ package day6
 
 import util.readFile
 
+var DIGIT_REGEX = ("""\d+""").toRegex()
+
+data class Race(
+    val time: Int,
+    val distance: Int,
+) {
+    fun calculateWiningOptions(): Int {
+        var winingOption = 0
+        for (t in 1 until distance) {
+            val remainingTime = time - t
+            if ((remainingTime * t) > distance) {
+                winingOption += 1
+            }
+        }
+        return winingOption
+    }
+}
+
 fun main() {
 
-    fun part1(lines: List<String>): Long = Long.MAX_VALUE
+    fun part1(lines: List<String>): Int {
+        val timeList = DIGIT_REGEX.findAll(lines[0]).map { it.value.toInt() }.toList()
+        val distanceList = DIGIT_REGEX.findAll(lines[1]).map { it.value.toInt() }.toList()
+
+        val raceLists = timeList.mapIndexed { index, t -> Race(t, distanceList[index]) }
+
+        return raceLists.map { it.calculateWiningOptions() }.reduce { acc, i -> acc * i }
+    }
 
     val lines = readFile("day6/Part1")
     println("Part One: ${part1(lines)}")
