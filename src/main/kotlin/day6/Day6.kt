db59d37100313f47edc315e103c90a64193aad8c
Emre Alkan <alkan@harver.com>
2023-12-13 00:44:53 +0100

    AoC2023 Day6/Part2

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -5,16 +5,18 @@ import util.readFile
 var DIGIT_REGEX = ("""\d+""").toRegex()
 
 data class Race(
-    val time: Int,
-    val distance: Int,
+    val time: Long,
+    val distance: Long,
 ) {
-    fun calculateWiningOptions(): Int {
-        var winingOption = 0
+    fun calculateWiningOptions(): Long {
+        var winingOption = 0L
         for (t in 1 until distance) {
             val remainingTime = time - t
             if ((remainingTime * t) > distance) {
-                winingOption += 1
+                winingOption += 1L
             }
+
+            if(remainingTime < 0) break
         }
         return winingOption
     }
@@ -22,15 +24,23 @@ data class Race(
 
 fun main() {
 
-    fun part1(lines: List<String>): Int {
-        val timeList = DIGIT_REGEX.findAll(lines[0]).map { it.value.toInt() }.toList()
-        val distanceList = DIGIT_REGEX.findAll(lines[1]).map { it.value.toInt() }.toList()
+    fun part1(lines: List<String>): Long {
+        val timeList = DIGIT_REGEX.findAll(lines[0]).map { it.value.toLong() }.toList()
+        val distanceList = DIGIT_REGEX.findAll(lines[1]).map { it.value.toLong() }.toList()
 
         val raceLists = timeList.mapIndexed { index, t -> Race(t, distanceList[index]) }
 
         return raceLists.map { it.calculateWiningOptions() }.reduce { acc, i -> acc * i }
     }
 
-    val lines = readFile("day6/Part1")
+    fun part2(lines: List<String>): Long {
+        val time = DIGIT_REGEX.findAll(lines[0]).map { it.value }.joinToString("").toLong()
+        val distance = DIGIT_REGEX.findAll(lines[1]).map { it.value }.joinToString("").toLong()
+
+        return Race(time, distance).calculateWiningOptions()
+    }
+
+    val lines = readFile("day6/Part1&2")
     println("Part One: ${part1(lines)}")
+    println("Part Two: ${part2(lines)}")
 }
