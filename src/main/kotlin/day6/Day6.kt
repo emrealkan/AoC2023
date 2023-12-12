b20fb9ab8be2223529da7a4a54dc4cb30d4331d8
Emre Alkan <alkan@harver.com>
2023-12-12 23:48:28 +0100

    AoC2023 Day6/Part1 Input file has been added

---
+++ src/main/kotlin/day6/Day6.kt
@@ -0,0 +1,11 @@
+package day6
+
+import util.readFile
+
+fun main() {
+
+    fun part1(lines: List<String>): Long = Long.MAX_VALUE
+
+    val lines = readFile("day6/Part1")
+    println("Part One: ${part1(lines)}")
+}
