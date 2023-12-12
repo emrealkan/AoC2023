d3b90b844a7b68cf13edcbc59e8fc9d5bc0667d5
Emre Alkan <alkan@harver.com>
2023-12-13 00:47:56 +0100

    AoC2023 Day7 files has been initialized

---
+++ src/main/kotlin/day7/Day7.kt
@@ -0,0 +1,13 @@
+package day7
+
+import util.readFile
+fun main() {
+
+    fun part1(lines: List<String>): Long {
+
+        return Long.MAX_VALUE
+    }
+
+    val lines = readFile("day7/Part1")
+    println("Part One: ${part1(lines)}")
+}
