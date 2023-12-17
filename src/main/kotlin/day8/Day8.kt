ab03d8586602a95f40a2797359088d46758ebb5a
Emre Alkan <alkan@harver.com>
2023-12-17 02:52:02 +0100

    AoC2023 Day8/Part2

--- src/main/kotlin/day8/Day8.kt
+++ src/main/kotlin/day8/Day8.kt
@@ -16,7 +16,7 @@ fun main() {
         }
     }
 
-    fun calculateTotalDirection(
+    fun calculateTotalDirectionPart1(
         navigationCommands: List<Int>,
         nodeCommands: List<Pair<String, List<String>>>,
     ): Int {
@@ -24,7 +24,7 @@ fun main() {
         var totalSteps = 0
 
         while (startPoint != "ZZZ") {
-            val directions = nodeCommands.find{ t -> t.first.trim() == startPoint }!!.second
+            val directions = nodeCommands.find { t -> t.first.trim() == startPoint }!!.second
             val direction = navigationCommands[totalSteps.mod(navigationCommands.size)]
 
             startPoint = directions[direction]
@@ -34,12 +34,58 @@ fun main() {
         return totalSteps
     }
 
+    fun gcd(a: Long, b: Long): Long {
+        var num1 = a
+        var num2 = b
+        while (num2 != 0L) {
+            val temp = num2
+            num2 = num1 % num2
+            num1 = temp
+        }
+        return num1
+    }
+
+    fun lcm(first: Long, second: Long): Long {
+        return first * second / gcd(first, second)
+    }
+
+    fun calculateTotalDirectionPart2(
+        navigationCommands: List<Int>,
+        nodeCommands: List<Pair<String, List<String>>>,
+    ): Long {
+        val startPoints = nodeCommands.filter { t -> t.first.trim().endsWith('A') }
+
+        return startPoints.map { t ->
+            var startPoint = t.first.trim()
+            var totalSteps = 0L
+
+            while (!startPoint.endsWith('Z')) {
+                val directions = nodeCommands.find { it.first.trim() == startPoint }!!.second
+                val direction = navigationCommands[totalSteps.mod(navigationCommands.size)]
+
+                startPoint = directions[direction]
+                totalSteps++
+            }
+
+            totalSteps
+        }.reduce { acc, i -> lcm(acc, i) }
+    }
+
     fun part1(lines: List<String>): Int {
         val navigationCommands = lines[0].map { if (it == 'L') 0 else 1 }
         val nodeCommands = getNodeCommands(lines)
-        return calculateTotalDirection(navigationCommands, nodeCommands)
+
+        return calculateTotalDirectionPart1(navigationCommands, nodeCommands)
     }
 
-    val lines = readFile("day8/Part1")
+    fun part2(lines: List<String>): Long {
+        val navigationCommands = lines[0].map { if (it == 'L') 0 else 1 }
+        val nodeCommands = getNodeCommands(lines)
+
+        return calculateTotalDirectionPart2(navigationCommands, nodeCommands)
+    }
+
+    val lines = readFile("day8/Part1&2")
     println("Part One: ${part1(lines)}")
+    println("Part Two: ${part2(lines)}")
 }
