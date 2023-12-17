bb91a90e56872ebb13063b32521a293208c5d9e1
Emre Alkan <alkan@harver.com>
2023-12-17 02:55:16 +0100

    AoC2023 Day9 files has been initialized

---
+++ src/main/kotlin/day9/Day9.kt
@@ -0,0 +1,12 @@
+package day9
+
+import util.readFile
+
+fun main() {
+    fun part1(lines: List<String>): Int {
+        return Int.MAX_VALUE
+    }
+
+   val lines = readFile("day8/Part1")
+    println("Part One: ${part1(lines)}")
+}
