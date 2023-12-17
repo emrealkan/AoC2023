e67c7b2a0ef68917359fe1496cf4df73595acc70
Emre Alkan <alkan@harver.com>
2023-12-18 00:53:43 +0100

    AoC2023 Day9/Part2

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -9,11 +9,23 @@ fun main() {
     }
 
     fun part1(lines: List<String>): Long {
-        val mappedLines = lines.map{ line -> getNext(line.split(" ").map { it.toLong() }) }
+        val mappedLines = lines.map { line -> getNext(line.split(" ").map { it.toLong() }) }
 
         return mappedLines.sum()
     }
 
-   val lines = readFile("day9/Part1")
+    fun getPrevious(list: List<Long>): Long {
+        return if (list.all { it == 0L }) 0L
+        else list.first() - getPrevious(list.windowed(2).map { it[1] - it[0] })
+    }
+
+    fun part2(lines: List<String>): Long {
+        val mappedLines = lines.map { line -> getPrevious(line.split(" ").map { it.toLong() }) }
+
+        return mappedLines.sum()
+    }
+
+    val lines = readFile("day9/Part1&2")
     println("Part One: ${part1(lines)}")
+    println("Part Two: ${part2(lines)}")
 }
