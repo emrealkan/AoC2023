3322f53071c713a9f6d9795c434dd089be8de59d
Emre Alkan <alkan@harver.com>
2023-12-18 01:09:12 +0100

    AoC2023 Day11 files has been initialized

---
+++ src/main/kotlin/day11/Day11.kt
@@ -0,0 +1,12 @@
+package day11
+
+import util.readFile
+
+fun main() {
+    fun part1(lines: List<String>): Long {
+        return Long.MAX_VALUE
+    }
+
+    val lines = readFile("day11/Part1")
+    println("Part One: ${part1(lines)}")
+}
