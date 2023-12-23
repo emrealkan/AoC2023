3f64cd512f46b3c1497041e8cc1becd8b86750fa
Emre Alkan <alkan@harver.com>
2023-12-23 13:57:31 +0100

    AoC2023 Day15 files has been initialized

---
+++ src/main/kotlin/day15/Day15.kt
@@ -0,0 +1,12 @@
+package day15
+
+import util.readFile
+
+fun main() {
+    fun part1(lines: List<String>): Long {
+        return Long.MAX_VALUE
+    }
+
+    val lines = readFile("day15/Part1")
+    println("Part One: ${part1(lines)}")
+}
--- src/main/kotlin/util/FileUtil.kt
+++ src/main/kotlin/util/FileUtil.kt
@@ -2,5 +2,6 @@ package util
 
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.io.path.readText
 
 fun readFile(name: String) = Path("src/main/kotlin/$name").readLines()
